d42cecc658984696ef0e91f67b73848777fdd9e2
maronita <maronita@users.noreply.github.com>
2021-08-18 16:06:35 -0500

    Intro

---
+++ src/config/global.js
@@ -0,0 +1,108 @@
+export default {
+  creditos: {
+    liderEquipo: [
+      {
+        nombre: 'Maria Camila Garcia Santamaria',
+        cargo: 'Líder del equipo',
+        centro: 'Dirección General',
+      },
+    ],
+    contenidoInstruccional: [
+      {
+        nombre: 'Lizeth Daniela Reinoso',
+        cargo: 'Experto temático',
+        centro: 'Centro Agropecuario La Granja Espinal - Regional Tolima',
+      },
+      {
+        nombre: 'Leydy Jhuliana Jaramillo Mejía',
+        cargo: 'Diseñadora instruccional',
+        centro: 'Centro de Gestión Industrial - Regional Distrito Capital',
+      },
+      {
+        nombre: 'Oscar Absalón Guevara',
+        cargo: 'Evaluador instruccional',
+        centro: 'Centro de Gestión Industrial - Regional Distrito Capital ',
+      },
+      {
+        nombre: 'Rafael Neftalí Lizcano Reyes',
+        cargo: 'Asesor metodológico y pedagógico',
+        centro:
+          'Centro Industrial del Diseño y la Manufactura - Regional Santander',
+      },
+      {
+        nombre: 'Julia Isabel Roberto',
+        cargo: 'Diseñadora y evaluadora instruccional',
+        centro:
+          'Centro para la Industria de la Comunicación Gráfica - Regional Distrito Capital',
+      },
+    ],
+    desarrolloProducto: [
+      {
+        nombre: 'Francisco José Lizcano Reyes',
+        cargo: 'Responsable del equipo',
+        centro:
+          'Centro Industrial del Diseño y la Manufactura - Regional Santander',
+      },
+      {
+        nombre: 'Leyson Fabian Castaño Perez',
+        cargo: 'Soporte organizacional',
+        centro: 'Centro de Comercio y Servicios - Regional Tolima',
+      },
+      {
+        nombre: ['Jesús Antonio Vecino Valero'],
+        cargo: 'Diseño web',
+        centro:
+          'Centro Industrial del Diseño y la Manufactura - Regional Santander',
+      },
+      {
+        nombre: 'Camilo Villamizar Lizcano',
+        cargo: 'Producción audiovisual',
+        centro:
+          'Centro Industrial del Diseño y la Manufactura - Regional Santander',
+      },
+      {
+        nombre: 'Gilberto Junior Rodriguez Rodriguez',
+        cargo: 'Producción audiovisual',
+        centro:
+          'Centro Industrial del Diseño y la Manufactura - Regional Santander',
+      },
+      {
+        nombre: 'María Carolina Tamayo Lopez ',
+        cargo: 'Producción audiovisual',
+        centro:
+          'Centro Industrial del Diseño y la Manufactura - Regional Santander',
+      },
+      {
+        nombre: 'Wilson Andrés Arenales Caceres',
+        cargo: 'Producción audiovisual',
+        centro:
+          'Centro Industrial del Diseño y la Manufactura - Regional Santander',
+      },
+      {
+        nombre: 'Zuleidy María Ruiz Torres',
+        cargo: 'Producción audiovisual',
+        centro: 'Centro de Comercio y Servicios - Regional Tolima',
+      },
+      {
+        nombre: 'Gustavo Adolfo Marún Suárez',
+        cargo: 'Desarrollo front-end',
+        centro:
+          'Centro Industrial del Diseño y la Manufactura - Regional Santander',
+      },
+
+      {
+        nombre: 'Yenny Patricia Ulloa Villamizar',
+        cargo: 'Validación de diseño y contenido',
+        centro:
+          'Centro Industrial del Diseño y la Manufactura - Regional Santander',
+      },
+    ],
+    gestoresRepositorio: [
+      {
+        nombre: 'Milady Tatiana Villamil Castellanos',
+        cargo: 'Validación y vinculación en plataforma LMS',
+        centro: 'Centro de Comercio y Servicios - Regional Tolima',
+      },
+    ],
+  },
+}
---
+++ src/views/Inicio.vue
@@ -0,0 +1,197 @@
+<template lang="pug">
+section
+  .banner-principal.mb-5
+    .container.tarjeta.p-4.p-sm-5(
+      :style="{'background-image': `url(${require('@/assets/curso/images/header/fondo-banner-principal.svg')})`}"
+    )
+      .row.justify-content-around.align-items-center
+        .col-lg-7.col-xxl-6.banner-principal__info
+          .banner-principal__programa
+            .h1.mb-0 Apropiación de Herramientas de Gestión del Riesgo Agroclimático
+          
+          .h2 Información del programa
+          .banner-principal__datos
+            ul
+              li.mb-3.banner-principal__datos__item
+                p.h6.mb-0.me-3.banner-principal__datos__item__titulo Nombre del programa: 
+                p.banner-principal__datos__item__texto Apropiación de Herramientas de Gestión del Riesgo Agroclimático
+              li.mb-3.banner-principal__datos__item
+                p.h6.mb-0.me-3.banner-principal__datos__item__titulo Código: 
+                p.banner-principal__datos__item__texto 72312149 versión 1
+              li.mb-3.banner-principal__datos__item
+                p.h6.mb-0.me-3.banner-principal__datos__item__titulo Total Horas: 
+                p.banner-principal__datos__item__texto 48 Horas
+              li.mb-3.banner-principal__datos__item
+                p.h6.mb-0.me-3.banner-principal__datos__item__titulo Duración en semanas: 
+                p.banner-principal__datos__item__texto 4 Semanas
+              li.mb-3.banner-principal__datos__item
+                p.h6.mb-0.me-3.banner-principal__datos__item__titulo Modalidad: 
+                p.banner-principal__datos__item__texto virtual
+              li.mb-3.banner-principal__datos__item
+                p.h6.mb-0.me-3.banner-principal__datos__item__titulo 
+                .banner-principal__datos__item__texto
+              
+
+        .d-none.d-lg-block.col-lg-5
+          //- img(src="@/assets/curso/banner-princiapal.svg")
+          .video
+            | <iframe data-v-1f9e4816="" width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="allowfullscreen"></iframe>
+
+  .container.tarjeta.tarjeta__template--azul-claro.p-4.p-md-5.mb-5(data-aos="fade-up")
+    .titulo__template--a.mb-4
+      span.h4 1. Presentación
+
+    p.mb-3 ¿Ha escuchado hablar de los riesgos agroclimáticos? 
+  
+    p.mb-3 Aquí se informará qué son, cómo se pueden evitar y algunas de las acciones a implementar frente a las probabilidades de ocurrencia, para ello el Servicio Nacional de Aprendizaje – SENA presenta el curso Apropiación de herramientas de gestión del riesgo agroclimático, cuyo objetivo es establecer técnicas de reducción de riesgos y adaptación al cambio climático.
+    
+    p.mb-3 Conozca un poco de este…
+
+    p.mb-3 En este curso se busca alcanzar la competencia Diagnosticar el riesgo de desastres de acuerdo con metodologías y normatividad, la cual presenta dos resultados de aprendizaje, el resultado de aprendizaje RA 1. Caracterizar riesgos agroclimáticos, según área de estudio y normatividad a través del cual se consolidan los contenidos referidos a la gestión del riesgo, marco normativo, variables climáticas, y el resultado de aprendizaje RA 2. Proponer medidas de riesgo y adaptación al cambio climático, según caracterización, el cual evidencia los contenidos de planificación de la producción agropecuaria, reducción del riesgo y adaptación al cambio climático, y planificación en la gestión del riesgo agroclimático.
+
+    p.mb-3 De esta manera, se evidencian aprendizajes que pueden ser aplicados a las realidades de los diferentes contextos para la adecuada planificación frente a la gestión del riesgo agroclimático.
+
+    p.mb-3 Es importante tener claridad frente algunos de los conceptos que serán abordados en las temáticas del curso, estos son:
+
+    p.mb-3 <b>Amenaza:</b> es la probabilidad de ocurrencia de un suceso potencialmente desastroso durante un período de tiempo en un sitio dado. 
+
+    p.mb-3 <b>Vulnerabilidad:</b> es el grado de pérdida de un elemento o grupo de elementos, resultado de la probable ocurrencia de un suceso desastroso.
+
+    p.mb-3 <b>Riesgo:</b> es el grado de pérdidas esperadas que generan pérdidas humanas o daños a propiedades y efectos sobre la actividad económica, debido a la ocurrencia de un desastre.
+
+    p.mb-3 El aprendiz de este curso estará en la capacidad de fortalecer habilidades para:
+
+    p.mb-3 <b> Prevención:</b>
+
+    p - Crear conciencia sobre la GRD.
+
+    p - Establecer sistemas de alerta temprana.
+
+    p - Desarrollar tecnologías de reducción del riesgo.
+
+    p.mb-3 <b>Mitigación:</b>
+    
+    p - segurar calidad en los proyectos agrícolas.
+    p - Promover enfoques multisectoriales.
+    p.mb-3 - Promover tecnologías de reducción del riesgo y sistemas de pronóstico climático.
+
+
+    p.mb-3 <b>Recuperación después del desastre:</b>
+
+    p - Promover procesos de recuperación específicos en el sector.
+    p - Movilizar ayuda financiera como garantías y créditos a largo plazo.
+    p - Promover tecnologías mejoradas.
+
+
+
+
+  .container.tarjeta.tarjeta__template--azul-claro.p-4.p-md-5.mb-5(data-aos="fade-up")
+    .titulo__template--a.mb-4
+      span.h4 2. Justificación<br>del programa
+
+    p.mb-3 La agricultura en Colombia es uno de los pilares económicos más importantes, ya que mueve la economía del país y aporta a la canasta familiar de toda la población y aún más, promueve la importación, ya que el producto colombiano es de un sabor único y de excelente calidad, es muy apetecido en otros países como lo ha sido el café y el cacao que se comercializa por su excelente sabor. darle la atención correspondiente.
+
+    p.mb-3 La agricultura aporta al desarrollo del país, ayuda a la población rural a minimizar la pobreza, brinda oportunidades de mejora a la población del campo, así mismo, ayuda al desarrollo sostenible y a la seguridad alimentaria; sin embargo, no todo es bueno en este campo, ya que se han venido presentando inconvenientes con ciertas restricciones en el mercado y el atraso en tecnología, sumado a esto el cambio climático que se viene generando en el mundo. Esto ha limitado algunos avances en los procesos, ha disminuido la producción, ya que el área agropecuaria depende de estas condiciones en todas las etapas vegetativas para obtener una cosecha productiva. 
+
+    p.mb-3 Teniendo en cuenta lo anterior es importante conocer la historia y trayectoria de los terrenos y de los procesos que se han tenido con ellos. Pero, aún más importante tener los conocimientos claros actualizados bajo la normatividad y los protocolos que se van presentando frente a las necesidades del contexto. 
+
+    p.mb-3 El Ministerio de Agricultura y Desarrollo Rural con el apoyo de la FAO, de los productores agropecuarios, extensionistas, funcionarios gubernamentales y miembros de organizaciones de la sociedad civil, entre otros, han creado una plataforma llamada Agrokit, la cual ayudará a todo agricultor, entidad pública o privada a acceder a la información de la gestión del riesgo agroclimático, mediante la biblioteca virtual. 
+
+    p.mb-3 De esta manera el programa de gestión del riesgo agroclimático que ofrece el SENA busca formar en competencias para afrontar el riesgo, su reducción y el manejo de desastres; todo esto encaminado a orientar e identificar dichos escenarios del riesgo agroclimático, así se podrá hacer análisis, evaluación y seguimiento, reduciendo que los productores se expongan a pérdidas o emergencias. 
+
+    p.mb-3 Este curso se realiza para poder ayudar al campesino a minimizar los impactos en el campo, porque el campo se proyecta con el clima, para que así se puedan implementar acciones que vinculen la participación desde la prevención, la mitigación y la recuperación frente a los desastres naturales y el impacto que genera para la población y sus cultivos. 
+
+  .container.tarjeta.tarjeta__template--azul-claro.p-4.p-md-5.mb-5(data-aos="fade-up")
+    .titulo__template--a.mb-4
+      span.h4 3. Competencias a<br> desarrollar
+
+    p El curso está dirigido a profesionales, tecnólogos y técnicos en salud que requieren conocer elementos conceptuales y normativos para implementar procesos y procedimientos operativos básicos que garanticen una atención en salud integral, oportuna y de calidad a las víctimas de ataque con agentes químicos.
+
+  .container.tarjeta.tarjeta__template--azul-claro.p-4.p-md-5.mb-5(data-aos="fade-up")
+    .titulo__template--a.mb-4
+      span.h4 4. Estrategia<br>metodológica
+
+    p 260402010 - Diagnosticar riesgo de desastres de acuerdo con metodologías y normativa. 
+      
+  .container.tarjeta.tarjeta__template--azul-claro.p-4.p-md-5.mb-5(data-aos="fade-up")
+    .titulo__template--a.mb-4
+      span.h4 5. Perfil de <br> ingreso
+
+    p.mb-3 Modalidad virtual. 
+
+    p.mb-3 Se requiere que el aprendiz tenga dominio de elementos básicos en el manejo de herramientas informáticas y de comunicación como correo electrónico, chat, procesadores de texto, software para presentaciones, navegadores de Internet y, otros sistemas y herramientas tecnológicas necesarias para la formación en ambientes virtuales.
+
+  //- Créditos
+  .container.tarjeta.tarjeta__template--azul-claro.p-4.p-md-5.mb-5(data-aos="fade-up")
+    .titulo__template--a.mb-4
+      span.h4 6.	Estrategia metodológica
+
+    p.mb-3 La estrategia de este curso se enfoca en adquirir los conocimientos y habilidades de manera práctica que se articulen a las necesidades del aprendiz y su contexto, centrándose en el estudio autónomo, pero con la ayuda del instructor quien dará todas las pautas e información para irlo guiando durante el proceso y la duración de este escenario formativo. De esta manera se garantizará el aprendizaje por competencias, integrando los ambientes virtuales de aprendizaje como mediadores en este proceso. 
+
+    p.mb-3 Igualmente, se estimula en el aprendiz de manera permanente el querer saber más sobre los resultados de aprendizaje, sobre cómo mejorar los conocimientos y cómo evidenciarlos a partir de su propia realidad. Se busca que todo esto lo logre a través de la vinculación activa de las cuatro fuentes de información para la construcción de conocimiento:
+
+    p.p1 -	El instructor - tutor
+    p.p1 -	El entorno
+    p.p1 -	Las TIC
+    p.p1 -	El trabajo colaborativo
+
+  .container.tarjeta.tarjeta__template--azul-claro.p-4.p-md-5.mb-5(data-aos="fade-up")
+    .titulo__template--a.mb-4
+      span.h4 7. Creditos
+    .creditos
+      div(
+        v-for="(creditoKey, index) of Object.keys(creditosData)"
+        :class="index != Object.keys(creditosData).length -1 ? 'mb-4' : ''" 
+      )
+        .creditos__titulo {{configTitulos[creditoKey]}}
+        table
+          tbody
+            tr(
+              v-for="(item, idx) of creditosData[creditoKey]" 
+              :key="creditoKey+idx"
+            )
+              td.text-bold(colspan='2' v-html="renderText(item.nombre)")
+              td(colspan='2' v-html="renderText(item.cargo)")
+              td(colspan='3' v-html="renderText(item.centro)")
+
+  Footer
+
+</template>
+<script>
+export default {
+  name: 'Inicio',
+  data: () => ({
+    configTitulos: {
+      liderEquipo: 'ECOSISTEMA DE RECURSOS EDUCATIVOS DIGITALES',
+      contenidoInstruccional: 'CONTENIDO INSTRUCCIONAL',
+      desarrolloProducto:
+        'DISEÑO Y DESARROLLO DE RECURSOS EDUCATIVOS DIGITALES',
+      gestoresRepositorio: 'GESTORES DE REPOSITORIO',
+    },
+  }),
+  computed: {
+    creditosData() {
+      return this.$config.creditos
+    },
+  },
+  methods: {
+    renderText(textObj) {
+      let newText = ''
+      if (Array.isArray(textObj)) {
+        textObj.forEach((texto, index) => {
+          newText += (index ? '<br/>' : '') + texto
+        })
+      } else {
+        newText += textObj
+      }
+      return newText
+    },
+  },
+}
+</script>
+
+<style lang="sass">
+.creditos
+  td, th
+    border-color: $color-sistema-c
+</style>
